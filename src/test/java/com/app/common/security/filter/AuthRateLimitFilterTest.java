37c897e650dbe53162d9c39ed9fa861e662919f8
NguyenMinh1301 <nguyenminh1301.dev@gmail.com>
2026-05-20 15:37:16 +0700

    refactor(auth): consolidate named rate-limit rules into single endpointRules map

--- src/test/java/com/app/common/security/filter/AuthRateLimitFilterTest.java
+++ src/test/java/com/app/common/security/filter/AuthRateLimitFilterTest.java
@@ -64,10 +64,10 @@ class AuthRateLimitFilterTest {
         objectMapper = new ObjectMapper();
         properties =
                 new RateLimitProperties(
-                        LOGIN_RULE,
-                        FORGOT_RULE,
-                        RESEND_RULE,
                         Map.of(
+                                LOGIN_PATH, LOGIN_RULE,
+                                FORGOT_PATH, FORGOT_RULE,
+                                RESEND_PATH, RESEND_RULE,
                                 REGISTER_PATH, LOW_TRAFFIC_RULE,
                                 REFRESH_PATH, new Rule(30, 60),
                                 RESET_PATH, new Rule(5, 300),
