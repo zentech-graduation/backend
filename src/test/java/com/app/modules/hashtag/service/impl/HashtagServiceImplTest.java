d74278005c952ce8fda481c05a1cd994f68e965c
NguyenMinh1301 <nguyenminh1301.dev@gmail.com>
2026-06-21 23:00:28 +0700

    fix(test): update hashtag repository mock

--- src/test/java/com/app/modules/hashtag/service/impl/HashtagServiceImplTest.java
+++ src/test/java/com/app/modules/hashtag/service/impl/HashtagServiceImplTest.java
@@ -73,7 +73,7 @@ class HashtagServiceImplTest {
         UUID postId = UUID.randomUUID();
         UUID hashtagId = UUID.randomUUID();
         Hashtag hashtag = Hashtag.builder().id(hashtagId).name("spring").build();
-        when(hashtagRepository.findByNameIgnoreCase("spring")).thenReturn(Optional.of(hashtag));
+        when(hashtagRepository.findByName("spring")).thenReturn(Optional.of(hashtag));
 
         service.upsertHashtagsForPost(postId, List.of("#Spring"));
 
